937b90d6a88eb07f39de5d91a938d2a629346985
dimplejoon <76550158+dimplejoon@users.noreply.github.com>
2024-06-09 18:43:00 +0900

    Legal_Alien_app day1

---
+++ app/src/main/java/com/joon/koreanman_in_tokyo_app/auth/IntroActivity.kt
@@ -0,0 +1,15 @@
+package com.joon.koreanman_in_tokyo_app.auth
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.joon.koreanman_in_tokyo_app.R
+
+class IntroActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_intro)
+
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "KoreanMan_In_Tokyo_app"
+include(":app")
+ 
