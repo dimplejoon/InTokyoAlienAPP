9dfb9552c7fc377af682848da5c9daf7723f8ac2
dimplejoon <76550158+dimplejoon@users.noreply.github.com>
2024-06-17 22:03:52 +0900

    Legal_Alien_app day2

---
+++ app/src/main/java/com/joon/koreanman_in_tokyo_app/auth/JoinActivity.kt
@@ -0,0 +1,13 @@
+package com.joon.koreanman_in_tokyo_app.auth
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.joon.koreanman_in_tokyo_app.R
+
+class JoinActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_join)
+    }
+}
