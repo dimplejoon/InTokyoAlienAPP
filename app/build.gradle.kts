3404fcdaddc9849e452fe929f6e404fed3277cfa
dimplejoon <76550158+dimplejoon@users.noreply.github.com>
2024-09-27 23:17:50 +0900

    Legal_Alien_app day2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,7 +3,7 @@ import org.jetbrains.kotlin.storage.CacheResetOnProcessCanceled.enabled
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
-    id("com.google.gms.google-services") version "4.4.2" apply false
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -46,8 +46,10 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation(platform("com.google.firebase:firebase-bom:33.1.0"))
+    implementation(platform("com.google.firebase:firebase-bom:33.3.0"))
     implementation("com.google.firebase:firebase-analytics")
+    implementation("com.google.firebase:firebase-auth-ktx:23.0.0")
+    implementation("com.google.firebase:firebase-auth")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
--- app/src/main/java/com/joon/koreanman_in_tokyo_app/auth/JoinActivity.kt
+++ app/src/main/java/com/joon/koreanman_in_tokyo_app/auth/JoinActivity.kt
@@ -1,13 +1,95 @@
 package com.joon.koreanman_in_tokyo_app.auth
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.databinding.DataBindingUtil
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import com.joon.koreanman_in_tokyo_app.MainActivity
 import com.joon.koreanman_in_tokyo_app.R
+import com.joon.koreanman_in_tokyo_app.databinding.ActivityJoinBinding
 
 class JoinActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+
+    private lateinit var binding: ActivityJoinBinding
+
+    val MINIMUM_LENGTH = 8
+
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.activity_join)
+
+        auth = Firebase.auth
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_join)
+
+        binding.signupBtn.setOnClickListener {
+
+            var isGoToJoin = true
+
+            val email = binding.emailArea.text.toString()
+            val password = binding.passwordArea.text.toString()
+            val passwordCheck = binding.passwordCheckArea.text.toString()
+
+            // 値が空いているか確認
+            if(email.isEmpty()){
+                Toast.makeText(this,"emailを入力してください", Toast.LENGTH_LONG).show()
+                isGoToJoin = false
+            }
+
+            if(password.isEmpty()){
+                Toast.makeText(this,"passwordを入力してください", Toast.LENGTH_LONG).show()
+                isGoToJoin = false
+            }
+
+            if(passwordCheck.isEmpty()){
+                Toast.makeText(this,"passwordチェックを入力してください", Toast.LENGTH_LONG).show()
+                isGoToJoin = false
+            }
+
+            //パスワード入力チェック
+            if(password.equals(passwordCheck)) {
+                Toast.makeText(this, "passwordとpasswordチェックが異なります。", Toast.LENGTH_LONG).show()
+                isGoToJoin = false
+            }
+
+            //パスワード桁数チェック
+            if( MINIMUM_LENGTH <= password.length){
+                Toast.makeText(this, "passwordは８桁以上入力してください。", Toast.LENGTH_LONG).show()
+                isGoToJoin = false
+            }
+
+            if(isGoToJoin){
+                auth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(this) {task ->
+                    if(task.isSuccessful){
+                        Toast.makeText(this, "成功", Toast.LENGTH_LONG).show()
+
+                        val intent = Intent(this, MainActivity::class.java)
+                        intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TOP
+                        startActivity(intent)
+
+                    }else {
+                        Toast.makeText(this,"失敗", Toast.LENGTH_LONG).show()
+                    }
+                }
+            }
+
+        }
+
+
+
     }
 }
+
+
+
+
+
+
